1c49591ecead26fd9bf08748f4bdad213f43175c
Vlad Rudenko <raimnist@yandex.ru>
2025-04-19 22:44:07 +0300

    Скрипты заполнения, отображения и удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
-// @todo: Темплейт карточки
+//Темплейт карточки
+const cardTemplate = document.querySelector(`#card-template`).content;
 
-// @todo: DOM узлы
+//DOM узлы
+const container = document.querySelector(`.page`);
+const content = container.querySelector(`.content`);
+const places = content.querySelector(`.places`);
+const placesList = places.querySelector(`.places__list`);
 
-// @todo: Функция создания карточки
+//Функция создания карточки
+function createCard(values, del) {
+    const card = cardTemplate.querySelector(`.card`).cloneNode(true);
 
-// @todo: Функция удаления карточки
+    card.querySelector(`.card__image`).src = values.link;
+    card.querySelector(`.card__image`).alt = values.name;
+    card.querySelector(`.card__title`).textContent = values.name;
 
-// @todo: Вывести карточки на страницу
+    card.querySelector(".card__delete-button").addEventListener('click', () => {
+        del(card);
+    });
+    return card;
+}
+
+//Функция удаления карточки
+function deleteCard(element) {
+    element.remove();
+}
+
+//Вывести карточки на страницу
+initialCards.forEach(item => {
+    placesList.append(createCard(item, deleteCard));
+});
